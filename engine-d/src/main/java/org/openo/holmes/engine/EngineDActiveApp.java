678100773b8d66e3aaf1875930159fda692ca957
GuangrongFu <fu.guangrong@zte.com.cn>
2017-03-14 08:28:06 +0800

    Optimize the Registration Logic of MSB
    Change-Id: I5be5a069d389081fad2b4a6fae558799716d91b6
    Issue-ID: HOLMES-50
    Signed-off-by: GuangrongFu <fu.guangrong@zte.com.cn>

--- engine-d/src/main/java/org/openo/holmes/engine/EngineDActiveApp.java
+++ engine-d/src/main/java/org/openo/holmes/engine/EngineDActiveApp.java
@@ -15,11 +15,40 @@
  */
 package org.openo.holmes.engine;
 
+import io.dropwizard.setup.Environment;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
 import org.openo.dropwizard.ioc.bundle.IOCApplication;
+import org.openo.holmes.common.api.entity.ServiceRegisterEntity;
+import org.openo.holmes.common.config.MicroServiceConfig;
+import org.openo.holmes.common.utils.MSBRegisterUtil;
 
+@Slf4j
 public class EngineDActiveApp extends IOCApplication<EngineDAppConfig> {
 
     public static void main(String[] args) throws Exception {
         new EngineDActiveApp().run(args);
+    }
+
+    @Override
+    public void run(EngineDAppConfig configuration, Environment environment) throws Exception {
+        super.run(configuration, environment);
+
+        try {
+            new MSBRegisterUtil().register(initServiceEntity());
+        } catch (IOException e) {
+            log.warn("Micro service registry httpclient close failure", e);
+        }
+    }
+
+    private ServiceRegisterEntity initServiceEntity() {
+        ServiceRegisterEntity serviceRegisterEntity = new ServiceRegisterEntity();
+        serviceRegisterEntity.setServiceName("holmes-rule-mgmt");
+        serviceRegisterEntity.setProtocol("REST");
+        serviceRegisterEntity.setVersion("v1");
+        serviceRegisterEntity.setUrl("/openoapi/holmes-rule-mgmt/v1");
+        serviceRegisterEntity.setSingleNode(MicroServiceConfig.getServiceIp(), "9101", 0);
+        serviceRegisterEntity.setVisualRange("1");
+        return serviceRegisterEntity;
     }
 }
