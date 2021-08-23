5aac9dc424bf07b2d641667afae7c34c35505595
付光荣10144542 <fu.guangrong@zte.com.cn>
2021-08-23 10:54:03 +0800

    bugfix - DB init error due to single quotes
    Issue-ID: HOLMES-463
    Signed-off-by: 付光荣10144542 <fu.guangrong@zte.com.cn>
    Change-Id: Iac835bfb113b336a1d4b46db6014fa4c55779d31

--- engine-d-standalone/src/main/assembly/bin/initDB.sh
+++ engine-d-standalone/src/main/assembly/bin/initDB.sh
@@ -47,6 +47,12 @@ if [ -z `env | grep PGPASSWORD` ]; then
     need_unset=1
 fi
 
+# Escape the single quote to avoid cause errors when executing the scripts.
+password=`sed -rn "s/.+ ('.+') .+/\1/p" $main_path/dbscripts/postgresql/onap-holmes_engine-createobj.sql`
+password=${password:1:-1}
+password_e=`echo $password | sed "s#'#''#g"`
+sed -i "s#$password#$password_e#" $main_path/dbscripts/postgresql/onap-holmes_engine-createobj.sql
+
 psql -U "$user" -p "$port" -h "$host" -d "$dbname" -f $main_path/dbscripts/postgresql/onap-holmes_engine-createobj.sql
 psql -U "$user" -p "$port" -h "$host" -d "$dbname" --command 'select * from alarm_info;'
 sql_result=$?
